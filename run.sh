4ad18f82d63ebb38b1c10365ddf620a324c53c60
Ryan Wesslen <ryan@explosion.ai>
2023-06-20 17:15:58 -0400

    add mkconfig to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+# Make sure prodigy.json has appropriate environment variables
+python scripts/mkconfig.py
+
 # Small bit of code that downloads data, can be replaced with proper script
 # that fetches data from s3 or another storage source.
 wget https://raw.githubusercontent.com/explosion/prodigy-recipes/master/example-datasets/news_headlines.jsonl

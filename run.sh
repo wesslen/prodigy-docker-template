215d4f195bd9b7beb58422dcbd586e71d22fe1ad
Ryan Wesslen <ryan@explosion.ai>
2023-06-20 12:41:40 -0400

    add initial build for m1 mac

---
+++ run.sh
@@ -0,0 +1,9 @@
+# Small bit of code that downloads data, can be replaced with proper script
+# that fetches data from s3 or another storage source.
+wget https://raw.githubusercontent.com/explosion/prodigy-recipes/master/example-datasets/news_headlines.jsonl
+
+# Show the stats, just for the logs
+python -m prodigy stats
+
+# Start Prodigy
+python -m prodigy ner.manual nyt blank:en news_headlines.jsonl --label "COMPANY,PERSON"
